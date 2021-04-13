18017f2f6d0181cdeadf20a9e315dedbd6fd3ce8
Jared de Uriarte <jared.deuri@gmail.com>
2021-04-12 21:33:14 -0400

    typo in .destroy

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -113,7 +113,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
-  Product.destory({
+  Product.destroy({
     where: {
       id: req.params.id
     }
